eee61e305e137ee068829bd383b0e4ee019f57b1
Jonnier <jonniersalaverria@gmail.com>
2023-10-23 09:42:03 -0500

    Se traslada el proyecto a github

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const seccion = document.getElementById("oferta-ind", "img-categoria");
+seccion.addEventListener("mouseover", () => {
+  seccion.style.boxShadow = "0 0 20px rgba(0, 0, 0, 0.0)"; /* Ajusta los valores según tus preferencias */
+});
+seccion.addEventListener("mouseout", () => {
+  seccion.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.8)"; /* Ajusta los valores según tus preferencias */
+});
+
+
